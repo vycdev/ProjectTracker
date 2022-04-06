41628f5a9174072cf481693f49017b0c4fc33833
Sandu Victor <vyctor661@gmail.com>
2022-04-06 16:23:09 +0300

    copy data to clipboard button

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-// Month in JavaScript is 0-indexed (January is 0, February is 1, etc),
-// but by using 0 as the day it will give us the last day of the prior
-// month. So passing in 1 as the month number will return the last day
-// of January, not February
-const daysInMonth = (month: number, year: number) => {
-    return new Date(year, month, 0).getDate();
-};
-
-// July
-daysInMonth(7, 2009); // 31
-// February
-daysInMonth(2, 2009); // 28
-daysInMonth(2, 2008); // 29
 
 type HeaderProps = {
     noOfDays: number;
+};
+
+type OneDayProgress = {
+    type: "vacation" | "progress";
+    name: string;
+    date: number; // unix timestamp;
+    color: string;
+};
+
+const daysInMonth = (month: number, year: number) => {
+    return new Date(year, month, 0).getDate();
 };
 
 const TableHeader = (props: HeaderProps) => {
     const [columns, setColumns] = useState<JSX.Element[]>([]);
     useEffect(() => {
         let cols = [];
+        cols.push(<th>Project Name</th>);
         for (let i = 1; i <= props.noOfDays; i++) {
             cols.push(<th key={"headerColumn" + i}>{i}</th>);
         }
@@ -32,16 +31,51 @@ const TableHeader = (props: HeaderProps) => {
     return <tr>{columns}</tr>;
 };
 
-const Table = () => {
-    // tr th
-    // tr td
+const Table = (props: HeaderProps) => {
+    return (
+        <table>
+            <TableHeader noOfDays={props.noOfDays}></TableHeader>
+        </table>
+    );
 };
+
+const AddProject = () => {
+    return <></>;
+};
+
+const CopyDataToClipboard = () => {
+    const [copied, setCopied] = useState(false);
+
+    const copyData = () => {
+        const Data = window.localStorage.getItem("data");
+        navigator.clipboard.writeText(JSON.stringify(Data));
+        setCopied(true);
+
+        setTimeout(() => {
+            setCopied(false);
+        }, 2500);
+    };
+
+    return (
+        <button
+            onClick={copyData}
+            className={"CopyButton " + copied ? "copied" : "notcopied"}
+        >
+            {copied ? "Data Has Been Copied" : "Copy Data To Clipboard"}
+        </button>
+    );
+};
+
 const App = () => {
     return (
         <div className="App">
-            <table>
-                <TableHeader noOfDays={31}></TableHeader>
-            </table>
+            <AddProject></AddProject>
+            <Table noOfDays={31}></Table>
+            <h6>
+                Warning, deleting cookies will delete all of the data in here,
+                so keep that in mind.
+            </h6>
+            <CopyDataToClipboard></CopyDataToClipboard>
         </div>
     );
 };
